0a21925398b60cdff5f8071b1a9bfec06d2ef5f3
Manny <mhdiaz20@gmail.com>
2021-06-02 01:53:47 -0700

    Updated metadata

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,8 +30,10 @@ function Home(){
                     >
 
                     <Card.Img variant = "top" 
-                    src = "https://wordvice-wp-static.s3-ap-northeast-1.amazonaws.com/uploads/2019/01/LinkedIn_HP.jpg"
+                    src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fgeospatialmedia.s3.amazonaws.com%2Fwp-content%2Fuploads%2F2018%2F09%2FLinkedIN.png&f=1&nofb=1"
                     className = "Card-image"
+                    alt = "Linkedin logo"
+                    style = {{backgroundColor: 'white'}}
                     >
                     
                     </Card.Img>
@@ -55,8 +57,10 @@ function Home(){
                     >
 
                     <Card.Img variant = "top" 
-                    src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fhtxt.co.za%2Fwp-content%2Fuploads%2F2018%2F11%2Fgithub-logo.jpg&f=1&nofb=1"
+                    src = "https://2sxc.org/Portals/0/adam/Content/4IqBjx3pXEC7a7-fVX2GBQ/Image/github-logo.png?w=1080&scale=both&quality=50"
                     className = "Card-image"
+                    alt = "GitHub logo"
+                    style = {{backgroundColor: 'white'}}
                     >
                        
                     </Card.Img>
