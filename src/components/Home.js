abab412878af36a724639f9071267a1ba85b2621
Manny <mhdiaz20@gmail.com>
2021-06-06 22:07:57 -0700

    New redesign.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,17 @@ import React, {useState} from 'react';
 import manueldiaz from '../images/manueldiaz.JPG';
 import '../App.css';
 import {Alert, Container, Row, Col, Image, Card, Button} from 'react-bootstrap';
+import { Github, Linkedin } from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css'
 function AlertDismissible(){
 const [show, setShow] = useState(true);
     return(
         <>
-        <Alert show={show} variant="secondary">
-        <Alert.Heading className = "text-center">This website is still a work in progress!</Alert.Heading>
+        <Alert show={show} variant="secondary" >
+        <Alert.Heading className = 'text-center'>This website is still a work in progress!
+        </Alert.Heading>
         <div className="d-flex justify-content-center">
-          <Button onClick={() => setShow(false)} variant="outline-secondary">
-            Dismiss
-          </Button>
+        <Button onClick={() => setShow(false)} variant="light" className = 'd-flex justify-content-center'>Dismiss</Button>
         </div>
         </Alert>
         {/*<!-- this button isn't required
@@ -28,77 +28,46 @@ function Home(){
             <AlertDismissible />
             <header className = "App m-4">
             <h1 className = "fade-in"> Manuel Diaz </h1>
+
             <Image className = 'resize'  src = {manueldiaz} alt = "manuel diaz" fluid rounded/>
             </header>
-            
-            <Container class = "align-items-center mt-5" >
-                <Row>
+            <Card bg = 'dark m-3 flex'>
+                <Row className = 'g-0'>
                     <Col>
-                    
-                        
-                    
-                    <Card bg = 'dark'
-                    text = "muted">
-                    
-                    <a
-                    href="https://www.linkedin.com/in/manuel-diaz-dev/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    <Card.Img variant = "top" 
-                    src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fgeospatialmedia.s3.amazonaws.com%2Fwp-content%2Fuploads%2F2018%2F09%2FLinkedIN.png&f=1&nofb=1"
-                    className = "Card-image"
-                    alt = "Linkedin logo"
-                    style = {{backgroundColor: 'white'}}
-                    >
-                    </Card.Img>
-                    </a>
-                    <Card.Body>Connect with me on LinkedIn.</Card.Body>
-                    </Card>
+                        <Github size = {150} className = 'ml-5'></Github>
                     </Col>
-
                     <Col>
-                    <Card bg = 'dark'
-                    text = "muted">
-                        
-                    <a
-                    href="https://github.com/manueldiazdev"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-
-                    <Card.Img variant = "top" 
-                    src = "https://2sxc.org/Portals/0/adam/Content/4IqBjx3pXEC7a7-fVX2GBQ/Image/github-logo.png?w=1080&scale=both&quality=50"
-                    className = "Card-image"
-                    alt = "GitHub logo"
-                    style = {{backgroundColor: 'white'}}
-                    >
-                       
-                    </Card.Img>
-                    </a>
-                    <Card.Body>View my projects on GitHub!</Card.Body>
-                        
-                    </Card>
+                        <Card.Body>
+                            <Card.Title>Checkout my <Card.Link 
+                            href="https://github.com/manueldiazdev"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className = 'App-link'>GitHub</Card.Link>!</Card.Title>
+                            <Card.Text> For the most part, I open source everything I work on, even this website.</Card.Text>
+                        </Card.Body>
                     </Col>
+                    
                 </Row>
-                <Row className = "mt-5 g-0">
-                    <Card bg = 'dark'
-                    text = "muted"
-                    >
-                        <Card.Body>Get to know more about me at my personal website</Card.Body>
-                        <Card.Link 
-                        href="https://manueldiaz.tech"
-                        target="_blank"
-                        className="App-link"
-                        rel="noopener noreferrer"
-                        style = {{textAlign: 'center'}}>
-                            Link
-                        </Card.Link>
-                    </Card>
+            </Card>
+            <Card bg = 'dark m-3'>
+                <Row className = 'g-0'>
+                    <Col>
+                        <Linkedin size = {150}/>
+                    </Col>
+                    <Col>
+                        <Card.Body>
+                            <Card.Title>Connect with me on <Card.Link 
+                            href="https://www.linkedin.com/in/manuel-diaz-dev/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className = 'App-link'>LinkedIn</Card.Link>!</Card.Title>
+                            <Card.Text> Feel free to start a conversation or take a look at my work history.</Card.Text>
+                        </Card.Body>
+                    </Col>
+                    
                 </Row>
-            </Container>
+            </Card>
             </div>
-            
         
         
         
