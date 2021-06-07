d2a317206cee95a15ef0d510df8bfa73421737aa
Manny <mhdiaz20@gmail.com>
2021-06-07 00:52:15 -0700

    Added more social media and link to source code to the website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,17 @@ function Footer() {
     return (
 
             <footer className="App-header p-3" style = {{textAlign: "center"}}>
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" style = {{width: 'auto'}} alt="logo" />
             <p>         
             Consistently learning and developing this website with React Js and Bootstrap styling.
             </p>
+            <p>
+            Check out the source of this project <a
+            href="https://github.com/manueldiazdev/my-one-page-app/tree/master"
+            target="_blank"
+            rel="noopener noreferrer"
+            className = 'App-link'
+            >here</a> !</p>
         
         
             </footer>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import manueldiaz from '../images/manueldiaz.JPG';
 import '../App.css';
-import {Alert, Container, Row, Col, Image, Card, Button} from 'react-bootstrap';
-import { Github, Linkedin } from 'react-bootstrap-icons';
+import {Alert, Row, Col, Image, Card, Button} from 'react-bootstrap';
+import { Github, Linkedin, Instagram, Facebook, Window} from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css'
 function AlertDismissible(){
 const [show, setShow] = useState(true);
@@ -22,19 +22,21 @@ const [show, setShow] = useState(true);
     )
 }
 
+
 function Home(){
     return(
-            <div>
+        <div>
             <AlertDismissible />
             <header className = "App m-4">
             <h1 className = "fade-in"> Manuel Diaz </h1>
 
             <Image className = 'resize'  src = {manueldiaz} alt = "manuel diaz" fluid rounded/>
             </header>
-            <Card bg = 'dark m-3 '>
+            
+            <Card bg = 'dark' className = 'mt-5'>
                 <Row className = 'g-0'>
                     <Col>
-                        <Github size = {150} className = 'ml-5'></Github>
+                        <Github size = {200} className = 'Card-image'></Github>
                     </Col>
                     <Col>
                         <Card.Body>
@@ -49,10 +51,10 @@ function Home(){
                     
                 </Row>
             </Card>
-            <Card bg = 'dark m-3'>
+            <Card bg = 'dark' className = 'mt-5'>
                 <Row className = 'g-0'>
                     <Col>
-                        <Linkedin size = {150}/>
+                        <Linkedin size = {200}  className = 'Card-image'/>
                     </Col>
                     <Col>
                         <Card.Body>
@@ -62,6 +64,62 @@ function Home(){
                             rel="noopener noreferrer"
                             className = 'App-link'>LinkedIn</Card.Link>!</Card.Title>
                             <Card.Text> Feel free to start a conversation or take a look at my work history.</Card.Text>
+                        </Card.Body>
+                    </Col>
+                    
+                </Row>
+            </Card>
+            <Card bg = 'dark' className = 'mt-5'>
+                <Row className = 'g-0'>
+                    <Col>
+                        <Instagram size = {200}  className = 'Card-image'/>
+                    </Col>
+                    <Col>
+                        <Card.Body>
+                            <Card.Title>Follow me on <Card.Link 
+                            href="https://www.instagram.com/manueldiazdev/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className = 'App-link'>Instagram</Card.Link>!</Card.Title>
+                            <Card.Text> Take a quick look at a few photos of my interests and day to day life.</Card.Text>
+                        </Card.Body>
+                    </Col>
+                    
+                </Row>
+            </Card>
+
+            <Card bg = 'dark' className = 'mt-5'>
+                <Row className = 'g-0'>
+                    <Col>
+                        <Facebook size = {200}  className = 'Card-image'/>
+                    </Col>
+                    <Col>
+                        <Card.Body>
+                            <Card.Title>Friend me on <Card.Link 
+                            href="https://www.facebook.com/profile.php?id=100057354373534"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className = 'App-link'>Facebook</Card.Link>!</Card.Title>
+                            <Card.Text>Know what event i'll be at next, or maybe invite me to yours.</Card.Text>
+                        </Card.Body>
+                    </Col>
+                    
+                </Row>
+            </Card>
+
+            <Card bg = 'dark' className = 'mt-5'>
+                <Row className = 'g-0'>
+                    <Col>
+                        <Window size = {200}  className = 'Card-image'/>
+                    </Col>
+                    <Col>
+                        <Card.Body>
+                            <Card.Title>Learn more about me on <Card.Link 
+                            href="https://manueldiaz.tech"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className = 'App-link'>my website</Card.Link>!</Card.Title>
+                            <Card.Text>Take a break from social media and visit my personal webpage.</Card.Text>
                         </Card.Body>
                     </Col>
                     
